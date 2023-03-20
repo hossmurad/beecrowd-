1484140a435fdb2aafee3ac0e5cfbd3c09ede51e
Hoss_murad <122189791+hossmurad@users.noreply.github.com>
2023-03-20 20:56:46 +0600

    Create beecrowd 1060.c

---
+++ beecrowd 1060.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int count = 0;
+    float num;
+    for (int i=0;i<=5;i++)
+    {
+        scanf("%f",&num);
+        if(num>0)
+        {
+            count++;
+        }
+    }
+    printf("%d valores positivos\n",count);
+ 
+    return 0;
+}
