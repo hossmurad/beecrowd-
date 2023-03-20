0791e3921aae197fc3358b4e6a2185bd8c8ab758
Hoss_murad <122189791+hossmurad@users.noreply.github.com>
2023-03-20 20:41:19 +0600

    Create beecrowd 1067.c

---
+++ beecrowd 1067.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ int  value;
+ scanf("%d",&value);
+ for (int i=0;i<=value;i++)
+ {
+     if(i%2!=0)
+     {
+         printf("%d\n",i);
+     }
+ }
+ 
+    return 0;
+}
