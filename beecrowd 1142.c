43aa81b605283ab3aae43242c8623e220d712124
Hoss_murad <122189791+hossmurad@users.noreply.github.com>
2023-04-24 23:55:33 +0600

    Create beecrowd 1142.c

---
+++ beecrowd 1142.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n*4;i+=4)
+    {
+        printf("%d %d %d PUM\n",i,(i+1),(i+2),(i+3));
+
+    }
+    return 0;
+}
