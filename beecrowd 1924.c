9f2fece48be72b81a14db456956ac1bdb2941ec1
Hoss_murad <122189791+hossmurad@users.noreply.github.com>
2023-04-25 21:22:40 +0600

    Create beecrowd 1924.c

---
+++ beecrowd 1924.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   int n,i=1;
+   char sub[50000];
+   scanf("%d",&n);
+   while(i<=n)
+   {
+       fflush(stdin);
+       gets(sub);
+       i++;
+   }
+   printf("Ciencia da Computacao\n");
+    return 0;
+}
